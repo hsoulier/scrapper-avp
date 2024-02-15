d743241aabca2242cb4483b2067884c0234ea74e
Hippolyte <hi.soulier@gmail.com>
2024-02-15 10:03:31 +0100

    🔧 Feat: Config eslint + Prettier + README

--- README.md
+++ README.md
@@ -1,7 +1,29 @@
 # Todo scrap Pathé
 
 ## Steps
-- Get list theater of Paris (GET https://www.pathe.fr/api/cities?language=fr)
-- Get list shows for each theater (GET https://www.pathe.fr/api/cinema/[slug-name-cinema]/shows?language=fr)
-- Filter results of shows with flag "Avant-Première" | "Avant premiere + équipe" + isEarlyAVP
 
+- Get list theater of Paris (GET https://www.pathe.fr/api/cities?language=fr)
+- Get list shows for each theater (GET https://www.pathe.fr/api/cinema/[cinema-slug]/shows?language=fr)
+- Filter results of shows with flag "Avant-Première" | "Avant premiere + équipe" + isEarlyAVP
+- Get details of show (GET https://www.pathe.fr/api/show/[film-name-with-id]/showtimes/[cinema-slug]/[date (ex:2024-02-27)]?language=fr
+  )
+
+## Schema of data
+
+- name (string)
+- showId (string)
+- movieId (string)
+- cinemaName (string)
+- dateShow (string -> converted from date) -> date + hour
+- version (string-> VO | VOSTF | VF)
+- earlyType (AVP | AVPE)
+- linkMovie (string)
+- linkShow (string)
+- genre ?(string[])
+- duration ?(string)
+- source (string -> where come from the data)
+
+## TODO
+
+- Cli to get data
+  - Choose the provider to scrap data
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,53 @@
-import Table from "./components/table";
-import TablePathe from "./components/table-pathe";
+import { useState } from "react"
+import Table from "./components/table"
+import TablePathe from "./components/table-pathe"
 
 function App() {
+  const [tab, setTab] = useState<"ugc" | "pathe">("ugc")
+
   return (
-    <main className="bg-neutral-900 h-screen overflow-y-auto text-neutral-200">
-      <div className="mt-8 mx-auto container flex flex-col gap-12">
-        <div>
-          <h1 className="text-3xl font-bold">UGC Data AVP à Paris</h1>
-          <div className="my-4 rounded-xl border border-neutral-700">
-            <Table />
+    <main className="bg-neutral-900 h-screen overflow-y-auto text-neutral-200 w-screen">
+      <section className="mt-8 mx-8 flex flex-col gap-12">
+        <aside>
+          <h1 className="text-3xl font-bold">Cinémas</h1>
+          <div className="flex gap-4 mt-4">
+            <button
+              data-state={tab === "ugc" ? "selected" : "none"}
+              className="px-4 py-2 rounded-lg data-[state=selected]:bg-neutral-500 data-[state=selected]:text-neutral-100"
+              onClick={() => setTab("ugc")}
+            >
+              UGC
+            </button>
+            <button
+              data-state={tab === "pathe" ? "selected" : "none"}
+              className="px-4 py-2 rounded-lg data-[state=selected]:bg-neutral-500 data-[state=selected]:text-neutral-100"
+              onClick={() => setTab("pathe")}
+            >
+              Pathé
+            </button>
           </div>
-        </div>
-        <div>
-          <h1 className="text-3xl font-bold">Pathé Data AVP à Paris</h1>
-          <div className="my-4 rounded-xl border border-neutral-700">
-            <TablePathe />
-          </div>
-        </div>
-      </div>
+        </aside>
+        <article className="mb-8 w-full flex flex-col gap-2">
+          {tab === "ugc" && (
+            <>
+              <h1 className="text-xl font-bold">UGC Data AVP à Paris</h1>
+              <div className="rounded-xl border border-neutral-700">
+                <Table />
+              </div>
+            </>
+          )}
+          {tab === "pathe" && (
+            <>
+              <h1 className="text-xl font-bold">Pathé Data AVP à Paris</h1>
+              <div className="rounded-xl border border-neutral-700">
+                <TablePathe />
+              </div>
+            </>
+          )}
+        </article>
+      </section>
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
