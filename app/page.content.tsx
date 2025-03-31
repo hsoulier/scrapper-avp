22e71816b52a6c9d8ded68105650e59ab6a59581
hsoulier <hi.soulier@gmail.com>
2025-03-31 23:19:57 +0200

    📱 Fix: Responsive filters component

--- app/page.content.tsx
+++ app/page.content.tsx
@@ -30,7 +30,7 @@ export const Content = () => {
   })
 
   return (
-    <main className="mx-auto lg:px-5 max-w-screen-2xl gap-x-4 gap-y-6 grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))]">
+    <main className="mx-auto px-4 lg:px-5 max-w-screen-2xl gap-x-4 gap-y-6 grid grid-cols-2 md:grid-cols-[repeat(auto-fill,minmax(200px,1fr))]">
       {data?.map((movie) => (
         <MovieCard key={movie.movie_id} movie={movie} />
       ))}
